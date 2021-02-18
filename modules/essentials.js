282234d4da70a5a9dd74c67998ffa978d547b494
acedron <acedrons@yahoo.co.jp>
2021-02-19 00:19:28 +0300

    Get Prefix from Database.

---
+++ modules/essentials.js
@@ -0,0 +1,15 @@
+const default_guild_doc = {
+  prefix: '&',
+  logs_channel: '',
+  moderation_logs_channel: '',
+  autorole: ''
+};
+
+const default_user_doc = {
+  osu_profile: ''
+};
+
+module.exports = {
+  default_user_doc,
+  default_guild_doc
+};
