5cca8a5f0ff5450cf06078d9c9ad7abebd3f5ffe
acedron <acedrons@yahoo.co.jp>
2021-02-24 16:57:04 +0300

    registerCase function and moderation channel support.

--- modules/logger.js
+++ modules/logger.js
@@ -2,26 +2,44 @@ const Discord = require('discord.js');
 
 class Logger {
   static Cache = null;
+  static db = null;
 
   static loadCache(Cache) {
     this.Cache = Cache;
   }
 
-  static log(guild, embedData) {
-    if (guild !== null) Cache.getGuildData(guild.id)
+  static loadDatabase(db) {
+    this.db = db;
+  }
+
+  static log(guild, embedData, moderation = false) {
+    if (guild !== null) this.Cache.getGuildData(guild.id)
       .then(data => {
-        if (data.logs_channel) {
+        let channelId = moderation ? data.moderation_logs_channel : data.logs_channel;
+        if (channelId) {
           let embed = new Discord.MessageEmbed()
             .setColor('#85dbfc')
             .setTimestamp()
             .setAuthor(embedData.author ? embedData.author.tag : guild.name, embedData.author ? embedData.author.displayAvatarURL() : guild.iconURL())
             .setFooter(embedData.footer ? embedData.footer : 'log');
           if (embedData.fields) embed.addFields(embedData.fields);
-          guild.channels.get(data.logs_channel).send(embed)
+          guild.channels.get(channelId).send(embed)
             .catch(console.error);
         }
       })
       .catch(console.error);
+  }
+
+  static registerCase(guildId, moderationData) {
+    return new Promise((resolve, reject) => this.Cache.getModerationCases(guildId)
+      .then(data => {
+        let keys = Object.keys(data).sort((a, b) => (a - b));
+        let newId = keys[keys.length - 1] + 1;
+        this.db.collection('guilds').doc(guildId).collection('cases').doc(newId).set(moderationData)
+          .then(() => resolve(newId)
+          .catch(reject));
+      })
+      .catch(reject));
   }
 }
 
